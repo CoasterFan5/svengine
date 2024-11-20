f0d12bd93c334696bcfe7687af518625d96a9319
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2024-11-20 15:19:31 -0500

    yay

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,26 @@
+<nav class="wrap">
+	<div class="left">
+		<a class="title" href="/">Svengine</a>
+	</div>
+	<div class="right">
+		<a href="login">Login</a>
+	</div>
+</nav>
+
+<style lang="scss">
+	.wrap {
+		width: 100%;
+		height: 3rem;
+		padding: 0 10%;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-between;
+		background: var(--background);
+		border-bottom: 1px solid var(--primary);
+	}
+
+	a {
+		all: unset;
+	}
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,31 @@
+<script>
+	import Navbar from '$lib/components/Navbar.svelte';
+	import '@fontsource/monaspace-neon';
+	let { children } = $props();
+</script>
+
+<Navbar />
+{@render children()}
+
+<style lang="scss">
+	:global(body) {
+		--text: #252422;
+		--background: #ffffff;
+		--primary: #ffa100;
+		--secondary: #413e3a;
+		--accent: #ccc5b8;
+		margin: 0;
+
+		color: var(--text);
+		font-family: 'Monaspace Neon', monospace;
+	}
+
+	:global(*) {
+		box-sizing: border-box;
+		color: var(--text);
+	}
+
+	:global(*::selection) {
+		background: var(--primary);
+	}
+</style>
